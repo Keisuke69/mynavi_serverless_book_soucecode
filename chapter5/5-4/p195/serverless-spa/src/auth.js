48afddec391b6f078cb3fd9033a5aca10cdbfa61
nishikei <nishikei@amazon.co.jp>
2018-03-15 18:11:38 +0900

    P195までを追加

--- chapter5/5-4/p195/serverless-spa/src/auth.js
+++ chapter5/5-4/p195/serverless-spa/src/auth.js
@@ -61,4 +61,84 @@ export default {
                 });
         })
     },
+    authenticate: function (email, password) {
+        var _this = this
+        var authenticationData = {
+            Username: email,
+            Password: password,
+        };
+        var authenticationDetails = new AuthenticationDetails
+            (authenticationData);
+        var poolData = {
+            UserPoolId: appConfig.UserPoolId,
+            ClientId: appConfig.UserPoolClientId
+        };
+        var userPool = new CognitoUserPool(poolData);
+        var userData = {
+            Username: email,
+            Pool: userPool
+        };
+        var cognitoUser = new CognitoUser(userData);
+        return new Promise((resolve, reject) => {
+            cognitoUser.authenticateUser(authenticationDetails, {
+                onSuccess: function (result) {
+                    console.log('access token + ' + result.
+                        getAccessToken().getJwtToken());
+                    console.log('idToken + ' + result.idToken.
+                        jwtToken);
+                    console.log('Successfully logged in !');
+                    _this.onChange(true);
+                    resolve(result);
+                },
+                onFailure: function (err) {
+                    console.log(err);
+                    _this.onChange(false);
+                    reject(err);
+                },
+                newPasswordRequired: function (userAttributes,
+                    requiredAttributes) {
+                    var attributesData = {
+                        name: email
+                    };
+                    cognitoUser.completeNewPasswordChallenge
+                        ("Password1", attributesData, this)
+                }
+            })
+        })
+    },
+    loggedIn: function () {
+        var _this = this
+        var poolData = {
+            UserPoolId: appConfig.UserPoolId,
+            ClientId: appConfig.UserPoolClientId
+        };
+        var userPool = new CognitoUserPool(poolData);
+        var cognitoUser = userPool.getCurrentUser();
+        if (cognitoUser != null) {
+            cognitoUser.getSession(function (err, session) {
+                if (err) {
+                    console.log(err);
+                    return false;
+                }
+                console.log('session validity: ' + session.isValid());
+                cognitoUser.getUserAttributes(function (err,
+                    attributes) {
+                    if (err) {
+                        console.log(err);
+                        return false;
+                    } else {
+                        for (let i = 0; i < attributes.length; i++) {
+                            if (attributes[i].getName() == "email") {
+                                console.log('login user is ' +
+                                    attributes[i].getValue());
+                            }
+                        }
+                    }
+                });
+            });
+            return true;
+        } else {
+            return false;
+        }
+    },
 }
